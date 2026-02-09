9c3ea8f5e0322a54f2b0b5211161946ea3683f03
Praveen <pravkumar.dev@gmail.com>
2026-02-09 14:49:19 +0530

    font style changed

--- android/app/src/main/java/com/expensesplitter/app/HomeActivity.kt
+++ android/app/src/main/java/com/expensesplitter/app/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.expensesplitter.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,10 +12,21 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
+        val logout = findViewById<ImageView>(R.id.imgLogout)
+
         val sharedPref = getSharedPreferences("app_prefs", MODE_PRIVATE)
         val email = sharedPref.getString("email", "User")
 
-        val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
-        tvWelcome.text = "Welcome, $email"
+        tvWelcome.text = email
+
+        logout.setOnClickListener {
+            val sharedPref = getSharedPreferences("app_prefs", MODE_PRIVATE)
+            sharedPref.edit().clear().apply()
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
