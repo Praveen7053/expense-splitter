c621702489ad38212d49700b64aefd9d3b2cae4b
Praveen <pravkumar.dev@gmail.com>
2026-02-12 19:29:38 +0530

    load members - on group tab

--- android/app/src/main/java/com/expensesplitter/app/GroupDetailsActivity.kt
+++ android/app/src/main/java/com/expensesplitter/app/GroupDetailsActivity.kt
@@ -5,14 +5,23 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.expensesplitter.app.model.GroupMemberResponse
+import com.expensesplitter.app.network.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 class GroupDetailsActivity : AppCompatActivity() {
+
+    private lateinit var recyclerGroupMembers: RecyclerView
+    private var groupId: Long = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_details)
 
-        val groupId = intent.getLongExtra("GROUP_ID", -1)
+        groupId = intent.getLongExtra("GROUP_ID", -1)
         val groupName = intent.getStringExtra("GROUP_NAME")
 
         val tvTitle = findViewById<TextView>(R.id.tvGroupTitle)
@@ -29,5 +38,35 @@ class GroupDetailsActivity : AppCompatActivity() {
             addIntent.putExtra("GROUP_NAME", groupName)
             startActivity(addIntent)
         }
+
+        recyclerGroupMembers = findViewById(R.id.recyclerGroupMembers)
+        recyclerGroupMembers.layoutManager = LinearLayoutManager(this)
+
+        loadGroupMembers()
     }
+
+    private fun loadGroupMembers() {
+
+        RetrofitClient.getApiService(this)
+            .getGroupMembers(groupId)
+            .enqueue(object : Callback<List<GroupMemberResponse>> {
+
+                override fun onResponse(
+                    call: Call<List<GroupMemberResponse>>,
+                    response: Response<List<GroupMemberResponse>>
+                ) {
+                    if (response.isSuccessful) {
+                        recyclerGroupMembers.adapter =
+                            MemberAdapter(response.body() ?: emptyList())
+                    }
+                }
+
+                override fun onFailure(
+                    call: Call<List<GroupMemberResponse>>,
+                    t: Throwable
+                ) {
+                }
+            })
+    }
+
 }
